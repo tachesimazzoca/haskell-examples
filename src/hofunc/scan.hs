94ed709a27d048aeeab88a73c07776951edb9172
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2013-07-16 13:26:28 +0900

    Remove hofunc/*.hs

--- src/hofunc/scan.hs
+++
@@ -1,9 +0,0 @@
-{-
-  * Scan
-
-    ghci> listOfFiles ["var", "log", "httpd", "access_log"]
-    ["/var","/var/log","/var/log/httpd","/var/log/httpd/access_log"]
--}
-
-listOfFiles :: [String] -> [String]
-listOfFiles s = tail $ scanl (\acc x -> acc ++ "/" ++ x) [] s
