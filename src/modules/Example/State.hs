5dbd2508b3a853d59261d8889f4380a024e1ee37
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2013-07-30 17:49:27 +0900

    Add randomPoint function

--- src/modules/Example/State.hs
+++ src/modules/Example/State.hs
@@ -2,6 +2,7 @@
 module Example.State where
 
 import Control.Monad.State
+import System.Random
 
 -- |
 --
@@ -73,3 +74,32 @@ stackEvenFirst = do
     Just x  -> push $ if even x then x else x + 1
     Nothing -> push 0
   return ()
+
+-- |
+--
+-- >>> fst $ runState (randomPoint 1 10) (mkStdGen 1)
+-- (7,10)
+-- >>> fst $ runState (randomPoint (-5) 5) (mkStdGen 1)
+-- (-3,3)
+randomPoint :: Int -> Int -> State StdGen (Int, Int)
+randomPoint a b = do
+  let m = state random :: (State StdGen Int)
+  x <- m
+  y <- m
+  let f n = n `mod` (abs a + abs b) + a
+  return (f x, f y)
+
+-- |
+--
+-- >>> randomPoint' 1 10 (mkStdGen 1)
+-- (7,10)
+-- >>> randomPoint' (-5) 5 (mkStdGen 1)
+-- (-3,3)
+randomPoint' :: Int -> Int -> StdGen -> (Int, Int)
+randomPoint' a b g1 =
+  let
+    (x, g2) = random g1 :: (Int, StdGen)
+    (y, _)  = random g2 :: (Int, StdGen)
+    f n = n `mod` (abs a + abs b) + a
+  in
+    (f x, f y)
