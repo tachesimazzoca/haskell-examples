d756dd8d6bdf13ec7c4758563f4be9726b45d4f0
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2013-07-31 15:21:58 +0900

    Tell operation messages by using the Writer Monad

--- src/executables/rpn.hs
+++ src/executables/rpn.hs
@@ -1,26 +1,42 @@
 module Main (main) where
 
+import Control.Monad
+import Control.Monad.Writer
 import System (getArgs)
 
 main :: IO ()
 main = do
   args <- getArgs
-  print $ calc args
+  print $ runWriter $ foldM solveRPN (Just []) args
 
 -- | Calc in Reverse Polish Notation.
 --
--- >>> calc []
--- 0
--- >>> calc ["1", "2", "+"]
--- 3
--- >>> calc ["3", "5", "1", "2", "+", "-", "*"]
--- 6
--- >>> calc ["NaN", "12", "-"]
--- -12
-calc :: [String] -> Int
-calc xs = head $ foldl f [] xs ++ [0]
-  where
-    f (x:y:xs) "+" = (y + x) : xs
-    f (x:y:xs) "-" = (y - x) : xs
-    f (x:y:xs) "*" = (y * x) : xs
-    f xs x = (fst . head $ (reads x :: [(Int, String)]) ++ [(0, "")]) : xs
+-- >>> runWriter $ solveRPN (Just []) ""
+-- (Nothing,[])
+-- >>> runWriter $ solveRPN (Just [1, 2]) "+"
+-- (Just [3],["2 + 1"])
+-- >>> runWriter $ foldM solveRPN (Just []) ["3", "5", "1", "2", "+", "-", "*"]
+-- (Just [6],["1 + 2","5 - 3","3 * 2"])
+-- >>> runWriter $ foldM solveRPN (Just []) ["12", "Nan", "-"]
+-- (Nothing,["Parse error: Nan"])
+solveRPN :: Maybe [Int] -> String -> Writer [String] (Maybe [Int])
+solveRPN Nothing _ = return Nothing
+solveRPN _ ""      = return Nothing
+solveRPN (Just (x:y:ys)) "+" = do
+  tell [show y ++ " + " ++ show x]
+  return (Just ((y + x):ys))
+solveRPN (Just (x:y:ys)) "-" = do
+  tell [show y ++ " - " ++ show x]
+  return (Just ((y - x):ys))
+solveRPN (Just (x:y:ys)) "*" = do
+  tell [show y ++ " * " ++ show x]
+  return (Just ((y * x):ys))
+solveRPN (Just ys) y = do
+  let m = case (reads y :: [(Int, String)]) of
+            [(a, "")] -> Just a
+            _         -> Nothing
+  case m of
+    Just a  -> return ()
+    Nothing -> tell ["Parse error: " ++ y]
+
+  return (liftM (:ys) m)
